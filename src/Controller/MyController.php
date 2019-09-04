73d066be548849b2eb885db014ca897983b323b8
rafalmalenta <rmalenta@wp.pl>
2019-09-04 14:09:45 +0200

    added annotations and Abstractcontroller

---
+++ src/Controller/MyController.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+    class MyController extends AbstractController{
+        /**
+         * @Route("/")
+         * 
+         */
+        public function welcome(){
+            return $this->render('base.html.twig');
+        }
+    }
